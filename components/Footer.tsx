da0365a11c6ffefb046a12c301367cdf55950363
RCCC <rcristhiancc@gmail.com>
2023-06-16 16:11:26 -0400

    Modified Footer.tsx. Added link to home page

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,18 +1,38 @@
-import React from 'react'
+import React from "react";
 
-type Props = {}
+type Props = {};
 
-function Footer({ }: Props) {
-    return (
-        <div
-            className='flex flex-col border-t mt-1 pt-2 items-center text-sm bg-gray-100'
+function Footer({}: Props) {
+  return (
+    <div className="flex flex-col border-t mt-1 pt-2 items-center text-sm bg-gray-100">
+      <p>
+        2022-2023{" "}
+        <a
+          className="text-blue-500"
+          target="blank"
+          href="https://rccc-resume1.web.app/"
         >
-            <p>2022 RCCC 😎 Ebay-react with NFT demo</p>
-            <h1 className='font-bold'>Disclaimer</h1>
-            <h1>Ebay Logo obtained from <a className='text-blue-500' target='blank' href='https://commons.wikimedia.org/wiki/File:EBay_logo.svg'>Wikimedia commons</a></h1>
-            <h1>This is a demo page implemented with Next.js, tailwindcss, thirdweb among other libraries for testing purposes only.</h1>
-        </div>
-    )
+          RCCC
+        </a>{" "}
+        😎 Ebay-react with NFT demo
+      </p>
+      <h1 className="font-bold">Disclaimer</h1>
+      <h1>
+        Ebay Logo obtained from{" "}
+        <a
+          className="text-blue-500"
+          target="blank"
+          href="https://commons.wikimedia.org/wiki/File:EBay_logo.svg"
+        >
+          Wikimedia commons
+        </a>
+      </h1>
+      <h1>
+        This is a demo page implemented with Next.js, tailwindcss, thirdweb
+        among other libraries for testing purposes only.
+      </h1>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
